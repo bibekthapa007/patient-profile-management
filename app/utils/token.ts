383c74a3cac38c3bf4ffa6d7a9499f16b17c3a5f
bibekthapa007 <bibekthapa922@gmail.com>
2023-01-16 21:46:21 +0545

    Add token utils

---
+++ app/utils/token.ts
@@ -0,0 +1,11 @@
+export const createBrearerRefreshToken = () => {
+  const token = localStorage.getItem("refreshToken");
+
+  return `Bearer ${token}`;
+};
+
+export const createBrearerAccessToken = () => {
+  const token = localStorage.getItem("accessToken");
+
+  return `Bearer ${token}`;
+};
