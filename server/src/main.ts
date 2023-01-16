1fdff81f575e965c30f517fe804241b15dd7fbbb
bibekthapa007 <bibekthapa922@gmail.com>
2023-01-16 21:48:21 +0545

    Add cors on server

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,10 @@ import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(`Server started on port ${process.env.PORT}`);
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT);
 }
