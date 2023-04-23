d57fab7dfef99efb6eadabd3956ab3bb9fe7786d
bibekthapa007 <bibekthapa922@gmail.com>
2023-04-23 12:34:13 +0545

    Add swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +13,17 @@ async function bootstrap() {
     origin: ['http://localhost:3000'],
   });
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Zendenta api')
+    .setDescription('The Zendenta API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
